65c0196ab42862633894e0323f4bdb74bfc3b38d
Sayantan Mishra <sayanmdn@gmail.com>
2020-09-21 18:17:24 +0530

    form complete

--- lite/script.js
+++ lite/script.js
@@ -1,3 +1,18 @@
 validateForm = () => {
+    var x = document.forms["myForm"]["name"].value;
+    if (x == "abc" || x == "asd") {
+        alert("Name must be filled out");
+        return false;
+    }
+    var y = document.forms["myForm"]["number"].value;
+    // alert(typeof(y))
 
+    var phoneno = /^\d{10}$/;
+    if (y.match(phoneno)) {
+        return true;
+    }
+    else {
+        alert("Phone number not valid. 10 digits must be there in a number");
+        return false;
+    }
 }
